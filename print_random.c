95c5249589dc47e872eb235762a50d5f111786f6
Alexander Ochmanowicz <alexander.ochmanowicz@temple.edu>
2024-09-01 22:32:21 -0400

    implemented randchar

--- print_random.c
+++ print_random.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 #include <time.h>
 
+#include "random.h"
 
-
-int main()
+void main()
 {
-	int a;
-
-	/* seed the randomizer */
-	srand( (unsigned)time(NULL) );
+	//seed the randomizer
+	srand((unsigned)time(NULL));
 
 	printf("Today's random word: ");
-	for(a=0;a<7;a++)
-		putchar( randchar() );
-	putchar('\n');
 
-	return(0);
+	for (int a = 0; a < 7; ++a)
+	{
+		putchar(randchar());
+	}
+
+	putchar('\n');
 }
